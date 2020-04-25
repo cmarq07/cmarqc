bd133b3958cd2fc6dc0ce65808d69ae78daeed33
cmarq07 <49878807+cmarq07@users.noreply.github.com>
2020-04-24 17:33:45 -0700

    Social Icon Repositioning
    Repositioning the Social Icons
    Edited Github Icons to fit better with the others

--- static/resume.js
+++ static/resume.js
@@ -3,17 +3,6 @@ var downloadButton = document.getElementById("resumeDownload");
 
 downloadButton.onclick = downloadPdf;
 
-window.onscroll = function() {
-  var d = document.documentElement;
-  var offset = (d.scrollTop + window.innerHeight) + 20;
-  var height = d.offsetHeight;
-  if (offset >= height) {
-    document.getElementById("socialsScroll").id = "socials";
-  } else {
-	document.getElementById("socials").id = "socialsScroll";
-  }
-};
-
 function downloadPdf() {
 	window.open("files/resume.pdf", "_blank");
 }
