d46e068d4a010e3b513f835dace14a42b8dd4a28
cmarq07 <christiancalloway17@gmail.com>
2020-12-14 00:30:16 -0800

    Site Update

---
+++ static/about.js
@@ -0,0 +1,8 @@
+//Initializing variables
+var eportfolioButton = document.getElementById("eportfolio");
+
+eportfolioButton.onclick = eportfolioOpen;
+
+function eportfolioOpen() {
+	window.open("eportfolio.html", "_blank");
+}
